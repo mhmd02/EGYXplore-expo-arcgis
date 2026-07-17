029cb117ab6254b9b0ec37ecfb482d6afc0220b7
mhmd02 <mohammedashrafchiko@gmail.com>
2026-07-18 01:12:26 +0300

    Mock App: expo-arcgis Test Project

---
+++ config/arcgisExample.js
@@ -0,0 +1,13 @@
+export const ARCGIS_API_KEY = "";
+export const ARCGIS_LICENSE_KEY = "";
+
+export const FEATURE_LAYERS = {
+  metroStations: "",
+  metroLines: "",
+};
+
+export const MAP_CENTER = {
+  latitude: 30.0444,
+  longitude: 31.2357,
+  zoom: 11,
+};
